1e2bc442631da937e5cda123e41ee0f411ae90f5
RafayetHossen <hossenmdrafayet@gmail.com>
2025-05-04 16:09:24 +0600

    setup dynamic routing and tested

--- index.js
+++ index.js
@@ -13,6 +13,17 @@ app.get('/', (req, res) => {
     res.render("index");
 });
 
+app.get('/profile/:username', (req, res) => {
+    req.params.username = req.params.username.toUpperCase();
+    res.send(`<h1>Welcome ${req.params.username}</h1>`);
+});
+
+app.get("/author/:username/:age", (req, res) => {
+  req.params.age = req.params.age;
+  req.params.username = req.params.username;
+  res.send(`<h1>Welcome ${req.params.username} ${req.params.age}</h1>`);
+});
+
 app.listen(5100,()=>{
     console.log("Server is running on port 5100");
 })
